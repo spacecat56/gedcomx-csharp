fa90740743caf07b91df6ca99e713118a143a767
Tom Shanley <tom_shanley@msn.com>
2022-12-15 11:10:10 -0800

    commit again? Adopt one test into FIle project where it really belongs anyway

---
+++ GEDCOM X File Test Project 2/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GEDCOM_X_File_Test_Project_2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
